5e8b55e19cc2159a5c3a814f7ff96aa53a9db653
Massimiliano Baldo <bmaxium01@gmail.com>
2022-06-28 09:30:41 +0200

    Correct login field for User resource

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as okta from "@pulumi/okta";
 // Create a user
 const user: okta.user.User = new okta.user.User("user", {
     email: "hcurry@pulumi-okta.com",
-    login: "hcurry",
+    login: "hcurry@pulumi-okta.com",
     firstName: "Haskell",
     lastName: "Curry",
     // Fist time we create a user we need these parameters
