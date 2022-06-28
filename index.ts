37e91711b22e7f3f96e8f794d0c45b6a7dee2a9e
Massimiliano Baldo <bmaxium01@gmail.com>
2022-06-28 09:27:42 +0200

    Create User for OIDC application

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as okta from "@pulumi/okta";
+
+// Create a user
+const user: okta.user.User = new okta.user.User("user", {
+    email: "hcurry@pulumi-okta.com",
+    login: "hcurry",
+    firstName: "Haskell",
+    lastName: "Curry",
+    // Fist time we create a user we need these parameters
+    expirePasswordOnCreate: true,
+    password: "haskellIsBetter"
+});
