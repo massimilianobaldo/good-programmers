d5b02cb75bddc37aa788e2a962e393f9137a6c4b
Massimiliano Baldo <bmaxium01@gmail.com>
2022-07-09 11:54:59 +0200

    Adjust incorret setting for authentication_flow in the application

--- index.ts
+++ index.ts
@@ -5,10 +5,7 @@ const user: okta.user.User = new okta.user.User("user", {
     email: "hcurry@pulumi-okta.com",
     login: "hcurry@pulumi-okta.com",
     firstName: "Haskell",
-    lastName: "Curry",
-    // Fist time we create a user we need these parameters
-    expirePasswordOnCreate: true,
-    password: "haskellIsBetter"
+    lastName: "Curry"
 });
 
 // Create a group
@@ -23,15 +20,16 @@ const application: okta.app.OAuth = new okta.app.OAuth("application", {
     grantTypes: ["authorization_code"],
     redirectUris: ["https://example.com/"],
     type: "web",
+    tokenEndpointAuthMethod: "none",
 });
 
 // Create the server authentication
 const server: okta.auth.Server = new okta.auth.Server("authentication-server", {
     audiences: [application.id], //client id
-    description: "My Example Auth Server",
+    description: "An Authenticantion-Server for the Good Programmers",
     issuerMode: "ORG_URL",
     status: "ACTIVE",
-    name: "authentication-server",
+    name: "auth-server-good-programmers",
 });
 
 // Add the user to the group
