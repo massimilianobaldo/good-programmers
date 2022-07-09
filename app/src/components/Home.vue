d2574e114cd7dff66a6d595f57abcd74d1575bcf
Massimiliano Baldo <bmaxium01@gmail.com>
2022-07-09 14:35:22 +0200

    Create Home Component

---
+++ app/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Are you a Good Programmers? &#128064;</h1>
+    <h2>In order to discover it, you need to login using Okta</h2>
+    <div>
+        <button v-if="authState && authState.isAuthenticated" @click="logout">Logout</button>
+        <button v-else @click="login">Login</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'home',
+    methods: {
+        async login() {
+            await this.$auth.signInWithRedirect({ originalUri: '/' })
+        },
+        async logout() {
+            await this.$auth.signOut()
+        }
+    }
+})
+</script>
